f77aab9c1e9e803b6f632d6ce8c0dd8a0d97beb3
YangZhouhong <995933447@qq.com>
2019-01-24 12:24:59 +0800

    24

--- app/Log/error/2019-01/24.php
+++ app/Log/error/2019-01/24.php
@@ -217,3 +217,9 @@
 #2 /var/www/frame/myframe/public/index.php(6): spl_autoload_call('Bobby\\Component...')
 #3 {main}
 
+[2019-01-24 12:07:43]  ,error code: 404 error message:Route 404 Not Found in /var/www/frame/myframe/vendor/yangzhouhong/bobbys-oop/src/Component/Route/Base.php line 167
+#0 /var/www/frame/myframe/app/Core/Application.php(188): Bobby\Component\Route\Base->findRoute('GET', '/example/')
+#1 /var/www/frame/myframe/app/Core/Application.php(169): App\Core\Application->findRoute(Object(Bobby\Component\Http\Request\Instance\Normal))
+#2 /var/www/frame/myframe/public/index.php(8): App\Core\Application->responsePipeline(Object(Bobby\Component\Http\Request\Instance\Normal))
+#3 {main}
+
--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 
 $app = require '../bootstrap/app.php';
 
-// $request = new \Bobby\Component\Http\Request;
 $request = \Bobby\Component\Http\Request\InstanceFactory::make();
 
 $app->responsePipeline($request);
