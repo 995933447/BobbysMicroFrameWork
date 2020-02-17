3b187ed5244933d06e611c5b7db724a545a2996e
root <995933447@qq.com>
2020-02-18 01:13:53 +0800

    commit message

---
+++ app/Log/error/2019-02/21.php
@@ -0,0 +1,6 @@
+[2019-02-21 01:46:26]  ,error code: 404 error message:Route 404 Not Found in /var/www/html/BobbysMicroFrameWork/vendor/yangzhouhong/bobbysoop-simplify/src/Component/Route/Base.php line 168
+#0 /var/www/html/BobbysMicroFrameWork/app/Core/Application.php(188): Bobby\Component\Route\Base->findRoute('GET', '/kkk')
+#1 /var/www/html/BobbysMicroFrameWork/app/Core/Application.php(169): App\Core\Application->findRoute(Object(Bobby\Component\Http\Request\Instance\Normal))
+#2 /var/www/html/BobbysMicroFrameWork/public/index.php(7): App\Core\Application->responsePipeline(Object(Bobby\Component\Http\Request\Instance\Normal))
+#3 {main}
+
